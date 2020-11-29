1f9bd49ef50206725b7e8f9ad90a1cc24e75db4e
Jun <Junlim2748@gmail.com>
2020-11-29 21:14:47 +0800

    Events

--- EventManagementSystem/Global.asax.cs
+++ EventManagementSystem/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace EventManagementSystem
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        DBEntities db = new DBEntities();
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
